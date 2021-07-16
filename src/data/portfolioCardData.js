c79f7ea5a9a96026b3caab537243b3a4adca9853
Matthew Goad <matthewxgoad@gmail.com>
2021-07-15 20:24:59 -0500

    started portfolioData

---
+++ src/data/portfolioCardData.js
@@ -0,0 +1,15 @@
+const portfolioData = [
+    {
+        id: 1,
+        title: 'Password Generator',
+        description: `A minimal and very useful application that generates a strong password based on user defined criterion
+        like length and character types.`,
+        image: ['JavaScript', 'HTML', 'CSS'],
+        languages: 'request',
+        urlDeployed: 'https://matthewxgoad.github.io/password-generator/',
+        urlRepo: 'https://github.com/matthewxgoad/password-generator'
+    },
+    
+  ];
+  
+  export default portfolioData;
