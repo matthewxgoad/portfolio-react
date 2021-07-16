ba63c65441e8090d8a934ce3ea336c80bf6b02f8
Matthew Goad <matthewxgoad@gmail.com>
2021-07-15 21:23:32 -0500

    updated stylesheet linking; NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default function NavBar() {
+    return (
+        <>
+            <nav className="navbar is-fixed-top is-dark" role="navigation" aria-label="main navigation">
+                <div className="navbar-brand">
+                    <a className="navbar-item" href="./index.html">
+                        <img alt={"MG"} src="../images/mg-icon.svg" width="112" />
+                    </a>
+
+                    <div className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </div>
+                </div>
+                <div id="navbarBasicExample" className="navbar-menu has-text-right">
+                    <div className="navbar-start">
+                        <a href="#about" className="navbar-item">
+                            About
+                        </a>
+                        <a href="#portfolio" className="navbar-item">
+                            Portfolio
+                        </a>
+                        <a href="#contact" className="navbar-item">
+                            Contact
+                        </a>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
