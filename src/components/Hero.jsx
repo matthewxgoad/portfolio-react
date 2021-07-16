38bccdc1c7b53958aa646defd96f6a92958e5b64
Matthew Goad <matthewxgoad@gmail.com>
2021-07-15 21:31:01 -0500

    updated Hero element

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Hero.css';
+
+export default function Hero() {
+    return (
+        <section class="hero is-fullheight is-fullheight-with-navbar">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title is-1 has-text-warning has-background-dark">
+          Matt Goad makes things.
+        </h1>
+      </div>
+    </div>
+  </section>
+    )
+}
